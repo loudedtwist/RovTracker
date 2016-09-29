69cfc6c1027b09340c49e79698ed284e6545b99d
Dennis Wittchen <dennis.wittchen@informatik.htw-dresden.de>
2016-09-29 15:18:23 +0200

    refactor naming

--- app/src/main/kotlin/de/archaeonautic/rovtracker/Model/projectmodel/Location.kt
+++ app/src/main/kotlin/de/archaeonautic/rovtracker/Model/projectmodel/Location.kt
@@ -4,6 +4,6 @@ import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-open class Location(@PrimaryKey var id: Int, var name: String, var dates: RealmList<Date>) : RealmObject() {
-    constructor() : this(0, "", RealmList<Date>())
+open class Location(@PrimaryKey var id: Int, var name: String, var campaigns: RealmList<Campaign>) : RealmObject() {
+    constructor() : this(0, "", RealmList<Campaign>())
 }
